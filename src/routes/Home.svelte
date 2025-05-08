6e9ab807f3651917cd2ee7c307c9607134cdeab4
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-05-08 18:07:34 +0900

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -30,7 +30,7 @@
     <table class="table">
         <thead>
         <tr class="table-dark">
-            <th>번호들</th>
+            <th>번호</th>
             <th>제목들</th>
             <th>작성일시들</th>
         </tr>
